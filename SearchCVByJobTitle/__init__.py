c64fe96d590181b441e8e72905e14ee0774aad8b
mg5u19 <--your>
2022-01-08 16:39:53 +0200

    Changed names of the functions

---
+++ SearchCVByJobTitle/__init__.py
@@ -0,0 +1,30 @@
+import logging
+import json
+import azure.functions as func
+
+from SearchCVByJobTitle.db_operations import searchCvByJobTitle
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    jobTitle = req.params.get('jobTitle')
+    if not jobTitle:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            jobTitle = req_body.get('jobTitle')
+
+    if not jobTitle:
+        return func.HttpResponse(
+            "Enter Job Title",
+            status_code=400
+        )
+    else:
+        list = searchCvByJobTitle(jobTitle)
+        return func.HttpResponse(
+             json.dumps(list),
+             status_code=200
+        )
